1e5b2ce15cde48646ef38241621a25b530571f8a
Otabek <74847963+Otabekkadirov@users.noreply.github.com>
2021-08-22 18:28:56 +0500

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(number) {
+    if (number < 0) {
+        number = number * -1;
+    }
+    let reversed = 0;
+    while (number > 0) {
+        reversed = reversed * 10 + (number % 10);
+        number = Math.floor(number / 10);
+    }
+    return reversed;
+};
